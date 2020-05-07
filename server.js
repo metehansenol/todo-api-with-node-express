8a9803a91056077cee4d2fc788c29779cee42ffe
Metehan Senol <metehansenol@gmail.com>
2020-05-07 19:45:52 +0300

    cors middleware added

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const express = require('express');
 
+const cors = require('cors');
+
 const itemsRouter = require('./routes/items');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -15,7 +19,7 @@ app.use('/', function(req, res) {
 
 const server = http.createServer(app);
 
-const port = 3000;
+const port = 5000;
 
 server.listen(port);
 
